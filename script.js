b69c92c672cbc920a73f1ebe58d225309be49a5e
Suraj Nimbalkar <136218136+surajnimbalkar07@users.noreply.github.com>
2025-11-19 23:14:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-//your JS code here. If required.
-const allCodes=document.querySelectorAll(".code")
-allCodes[0].focus()
-allCodes.forEach((code,index)=>{
-	code.addEventListener("input",()=>{
-		const value=code.value;
-		if(value.length===1 && index<allCodes.length-1){
-			allCodes[index+1].focus();
-			
-		}
-	});
-	code.addEventListener("keydown",(e)=>{
-		if(e.key==="Backspace"){
-			if(code.value==="" && index>0){
-				allCodes[index-1].value="";
-			allCodes[index-1].focus()
-			}
-		}
-		
-	})
-})
+const allCodes = document.querySelectorAll(".code");
+
+allCodes[0].focus();
+
+allCodes.forEach((code, index) => {
+
+  code.addEventListener("input", () => {
+    const value = code.value;
+
+    if (value.length === 1 && index < allCodes.length - 1) {
+      allCodes[index + 1].focus();
+    }
+  });
+
+  code.addEventListener("keydown", (e) => {
+
+    if (e.key === "Backspace") {
+
+ 
+      if (code.value !== "") {
+        code.value = "";
+        return; 
+      }
+
+    
+      if (index > 0) {
+        allCodes[index - 1].value = "";
+        allCodes[index - 1].focus();
+      }
+    }
+
+  });
+
+});
